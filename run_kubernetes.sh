61a0616c8c26ae3bf63fabc46acb1fdf28f6d209
Vikas Bhatia <vikasb@Vikass-MBP.fios-router.home>
2021-03-15 22:17:24 -0400

    latest changes for setting infrastructure

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath=vvab13/apitest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run apitest-548b75c49-9c7jj --image=$dockerpath --port=80
+kubectl run apitest --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -16,4 +16,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pod/apitest-548b75c49-9c7jj 8000:80
+kubectl port-forward pod/apitest 8000:80
